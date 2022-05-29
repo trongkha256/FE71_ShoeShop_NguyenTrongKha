b2f2cdc5f67c067232b51a2d0b2ccab4b2063004
trongkha256 <trongkha256@gmail.com>
2022-05-29 17:40:59 +0700

    Store Shoe

--- src/component/ShoeStore.js
+++ src/component/ShoeStore.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import ProductList from './ProductList'
+import ProductItem from './ProductItem'
+import Modal from './Modal'
+import data from "./data.json"
 
 export default class ShoeStore extends Component {
     render() {
         return (
-            <div>ShoeStore</div>
+            <div className="container">
+                <h1 className="text-center">Shoes Shop</h1>
+                <ProductList products={data} />
+            </div>
         )
     }
 }
