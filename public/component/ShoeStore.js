49cf5b5af0b475ca7134229fa6288e9d9c4a2000
trongkha256 <trongkha256@gmail.com>
2022-05-29 17:17:35 +0700

    Add new component about Shoe Store

---
+++ public/component/ShoeStore.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ShoeStore extends Component {
+    render() {
+        return (
+            <div>ShoeStore</div>
+        )
+    }
+}
